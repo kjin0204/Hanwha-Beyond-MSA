6118d57eadef66a2b12f10c0c7de2829eeab9c06
HyeonggyuKang <kin0204@naver.com>
2025-09-23 13:12:00 +0900

    [MSA] gateway 예제

---
+++ chap02-01-firest-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.firstservice;
+
+public class FirstServiceController {
+}
---
+++ chap02-02-second-service-lecture-source/src/main/java/com/ohgiraffers/secondservice/SecondServiceController.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.secondservice;
+
+public class SecondServiceController {
+}
